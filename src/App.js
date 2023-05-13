45cf9def82e427a99038e30af8d1c666a065d474
Anderson23j <andersonvalenciaj23@gmail.com>
2023-05-12 19:11:23 -0500

    creacion nav y componentes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./componentes/NavBar";
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <NavBar />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../styles/main.css";
+function NavBar() {
+  const navRef = useRef();
+  const showNavar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+  return (
+    <header>
+      <h3>Taller M&A</h3>
+      <nav ref={navRef}>
+        <a href="/#">Inicio</a>
+        <a href="/#">Productos</a>
+        <a href="/#">Locales</a>
+        <a href="/#">Blog</a>
+        <a href="/#">Nosotros</a>
+        <a href="/#">Contacto</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+}
+
+export default NavBar;
