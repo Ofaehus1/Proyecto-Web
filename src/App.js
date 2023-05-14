5d89aed92c97f0c40c519cdf29837ae5d1e5cb31
Anderson23j <andersonvalenciaj23@gmail.com>
2023-05-14 14:03:00 -0500

    footer creado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import NavBar from "./componentes/NavBar";
 import "./App.css";
 import React from "react";
 import Inicio from "./componentes/inicio";
+import Footer from "./componentes/footer";
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
       <Inicio />
+      <Footer />
     </React.Fragment>
   );
 }
---
+++ src/componentes/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <small>Copyright © 2023 Taller M&A.</small>
+    </footer>
+  );
+}
+
+export default Footer;
